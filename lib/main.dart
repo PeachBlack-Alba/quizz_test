627c201bdfba1e8d22f7fbcf782e90e497f04a3d
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-05-19 10:28:24 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
 
-void main() {}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Text("Have a good day bebé!"),
     );
   }
 }
